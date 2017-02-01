38292ca22feaec8fab4a8ec6cf5933f8fe96755a
Todd Benson <todd.benson@csaa.com>
2017-02-01 14:48:26 -0700

    Cloud Foundry stuff

---
+++ src/test/hello/ApplicationTest.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gl42ben on 2/1/17.
+ */
+public class ApplicationTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
