10bd333dbb3fe2cf1f2d30212bd58e8fa9a6bb84
Todd Benson <todd.benson@csaa.com>
2017-04-03 09:55:03 -0700

    Update HelloWorldHttpController.java

--- src/main/java/hello/HelloWorldHttpController.java
+++ src/main/java/hello/HelloWorldHttpController.java
@@ -1,9 +1,5 @@
 package hello;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,18 +11,8 @@ public class HelloWorldHttpController {
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        String token = new Application().generateSecretToken();
-        System.out.print(token);
         return "greeting";
     }
     
-    @EnableWebSecurity
-    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-        }
-    }
 
 }
