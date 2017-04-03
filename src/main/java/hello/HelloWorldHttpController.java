f3c02ad7c8f755267057a7fb74031cf928052722
Todd Benson <todd.benson@csaa.com>
2017-04-03 11:18:03 -0700

    New branch commit

--- src/main/java/hello/HelloWorldHttpController.java
+++ src/main/java/hello/HelloWorldHttpController.java
@@ -1,6 +1,5 @@
 package hello;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldHttpController {
 
-
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         String token = new Application().generateSecretToken();
         System.out.print(token);
-        return "greeting";
+        return "greeting" + token;
     }
-
 }
