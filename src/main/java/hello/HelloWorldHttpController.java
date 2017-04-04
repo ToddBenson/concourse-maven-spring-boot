029553ab51bb76013adc8c46529d5654a7bf334a
Todd Benson <todd.benson@csaa.com>
2017-04-04 16:43:19 -0700

    New branch commit

--- src/main/java/hello/HelloWorldHttpController.java
+++ src/main/java/hello/HelloWorldHttpController.java
@@ -11,7 +11,7 @@ public class HelloWorldHttpController {
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        String newToken = new Application().generateSecretToken();
-        return "greeting" + newToken;
+//        String newToken = new Application().generateSecretToken();
+//        return "greeting" + newToken;
     }
 }
