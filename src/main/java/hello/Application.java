bb9763041246cf76dcbd65487eeb0caadd61b3c9
Todd Benson <todd.benson@csaa.com>
2017-04-04 15:52:21 -0700

    New branch commit

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,12 +2,18 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Random;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	String generateSecretToken(){
+		Random r = new Random();
+		return Long.toHexString(r.nextLong());
 	}
 
 }
