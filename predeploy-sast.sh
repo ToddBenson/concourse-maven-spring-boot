06e19cca8c9c30ed9b88acd23f06953ff59416b2
Todd Benson <todd.benson@csaa.com>
2017-04-06 18:17:50 -0700

    Update predeploy-sast.sh

--- predeploy-sast.sh
+++ predeploy-sast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 retire --path . --outputformat text --outputpath retire.txt
 eslint -c /opt/configs/static/.eslintrc --debug . -o ./eslint.txt
-./mvnw clean install findbugs:findbugs
-cat *.txt
+./mvnw clean install check:dependency-check
 sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.projectKey=$SONAR_PROJECT_KEY -Dsonar.projectName=$SONAR_PROJECT_NAME -Dsonar.projectVersion=$SONAR_PROJECT_VERSION -Dsonar.sources=$SONAR_SOURCES -Dsonar.java.binaries=$SONAR_JAVA_BINARIES -Dsonar.github.repository=$REPO -Dsonar.github.oauth=$GIT_REPO_TOKEN -Dsonar.login=$SONAR_TOKEN
+cat *.txt
