5231a834125ea9f041e80c385d7b4df06e4e564b
Todd Benson <todd.benson@csaa.com>
2017-04-12 11:24:12 -0700

    Merge branch 'master' of https://github.com/ToddBenson/concourse-maven-spring-boot
    # Conflicts:
    #	ci/bin/predeploy-sast.sh

--- ci/bin/predeploy-sast.sh
+++ ci/bin/predeploy-sast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 retire --path . --outputformat text --outputpath retire.txt
-eslint -c /opt/compliance-toolkit/static/.eslintrc --debug . -o ./eslint.txt
+eslint -c /opt/compliance-toolkit/config/static/.eslintrc --debug . -o ./eslint.txt
 ./mvnw clean install dependency-check:check
 sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.projectKey=$SONAR_PROJECT_KEY -Dsonar.projectName=$SONAR_PROJECT_NAME -Dsonar.projectVersion=$SONAR_PROJECT_VERSION -Dsonar.sources=$SONAR_SOURCES -Dsonar.java.binaries=$SONAR_JAVA_BINARIES -Dsonar.github.repository=$REPO -Dsonar.github.oauth=$GIT_REPO_TOKEN -Dsonar.login=$SONAR_TOKEN -Dsonar.dependencyCheck.reportPath=target/dependency-check-report.xml
 cat *.txt
