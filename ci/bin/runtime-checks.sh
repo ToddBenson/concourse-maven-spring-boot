c234e80e087615c214ca923c88e1aa3aa7fe03f6
Todd Benson <todd.benson@csaa.com>
2017-04-12 14:08:46 -0700

    Merge branch 'master' of https://github.com/ToddBenson/concourse-maven-spring-boot
    # Conflicts:
    #	ci/bin/predeploy-sast.sh

--- ci/bin/runtime-checks.sh
+++ ci/bin/runtime-checks.sh
@@ -32,36 +32,38 @@ echo $UNENCRYPT
 echo $ACCESS
 echo "----End"
 
-echo "Testing access over port 80..."
-$PYPATH/python ./checkhttps.py $SERVER > ./data/unencryptRun.txt
-if /usr/bin/diff ./data/unencryptRun.txt ./data/$UNENCRYPT > /dev/null
-then
-   echo "Access over unencrypted channels (port 80) remains the same."
-   $PYPATH/python ./sendtodatadog.py 0 https 0 $APP
-else
-   if [ "$verbose" == "0" ]
-   then
-     echo "Access over unencrypted channels (port 80) has changed. Run 'diff unencryptRun.txt unencryptControl.txt' to see details"
-   else
-     echo "Access to the following URL(s) over port 80 has changed. Please investigate and update the control file if necessary (unencryptControl.txt)."
-     /usr/bin/diff ./data/unencryptRun.txt ./data/$UNENCRYPT
-   fi
-   $PYPATH/python ./sendtodatadog.py 1 https 2 $APP
-fi
+echo "put runtime checks here"
 
-echo "Testing unauthenticated access..."
-$PYPATH/python ./checkaccess.py $SERVER > ./data/accessRun.txt
-if /usr/bin/diff ./data/accessRun.txt ./data/$ACCESS > /dev/null
-then
-   echo "Access to pages unauthenticated remains the same."
-   $PYPATH/python ./sendtodatadog.py 0 access 0 $APP
-else
-   if [ "$verbose" == "0" ]
-   then
-     echo "Access to pages unauthenticated has changed. Run 'diff accessRun.txt accessControl.txt' to see details."
-   else
-     echo "Access to the following URL(s) by an unauthenticated user has changed. Please investigate and update the control file if necessary (accessControl.txt)."
-     /usr/bin/diff ./data/accessRun.txt ./data/$ACCESS
-   fi
-   $PYPATH/python ./sendtodatadog.py 1 access 1 $APP
-fi
+#echo "Testing access over port 80..."
+#$PYPATH/python ./checkhttps.py $SERVER > ./data/unencryptRun.txt
+#if /usr/bin/diff ./data/unencryptRun.txt ./data/$UNENCRYPT > /dev/null
+#then
+#   echo "Access over unencrypted channels (port 80) remains the same."
+#   $PYPATH/python ./sendtodatadog.py 0 https 0 $APP
+#else
+#   if [ "$verbose" == "0" ]
+#   then
+#     echo "Access over unencrypted channels (port 80) has changed. Run 'diff unencryptRun.txt unencryptControl.txt' to see details"
+#   else
+#     echo "Access to the following URL(s) over port 80 has changed. Please investigate and update the control file if necessary (unencryptControl.txt)."
+#     /usr/bin/diff ./data/unencryptRun.txt ./data/$UNENCRYPT
+#   fi
+#   $PYPATH/python ./sendtodatadog.py 1 https 2 $APP
+#fi
+#
+#echo "Testing unauthenticated access..."
+#$PYPATH/python ./checkaccess.py $SERVER > ./data/accessRun.txt
+#if /usr/bin/diff ./data/accessRun.txt ./data/$ACCESS > /dev/null
+#then
+#   echo "Access to pages unauthenticated remains the same."
+#   $PYPATH/python ./sendtodatadog.py 0 access 0 $APP
+#else
+#   if [ "$verbose" == "0" ]
+#   then
+#     echo "Access to pages unauthenticated has changed. Run 'diff accessRun.txt accessControl.txt' to see details."
+#   else
+#     echo "Access to the following URL(s) by an unauthenticated user has changed. Please investigate and update the control file if necessary (accessControl.txt)."
+#     /usr/bin/diff ./data/accessRun.txt ./data/$ACCESS
+#   fi
+#   $PYPATH/python ./sendtodatadog.py 1 access 1 $APP
+#fi
