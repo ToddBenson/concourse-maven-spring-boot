faedd88fe1e18505f2fd1a28020fe738e34ac877
Todd Benson <todd.benson@csaa.com>
2017-04-12 15:53:14 -0700

    Merge branch 'master' of https://github.com/ToddBenson/concourse-maven-spring-boot
    # Conflicts:
    #	ci/bin/predeploy-sast.sh

--- ci/bin/build.sh
+++ ci/bin/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-./mvnw clean install
+#./mvnw clean install
 #/bin/cp ./target/*.war ../release
 
 set -e # fail fast
 set -x # print commands
 
 git clone deploy-file release
-
+cd git-repo
+./mvnw clean install
 cp ./target/*.war ../release
 cd ../release
 
